a29e6ec3e18f81d6f143602d1b54bed8f11c37dd
shundai.ogawa <ec2-user@ip-172-31-43-33.us-east-2.compute.internal>
2018-04-11 10:16:20 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  get 'signup', to: 'users#new'
+  resources :users, only: [:show, :new, :create]
 end
