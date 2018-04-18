184b84f713d4aa0603645003bced3dcc5805a26f
shundai.ogawa <ec2-user@ip-172-31-43-33.us-east-2.compute.internal>
2018-04-18 07:50:04 +0000

    saishuu

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,9 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "obog_#{Rails.env}"
+  
   config.action_mailer.perform_caching = false
+  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:               'smtp.gmail.com',
